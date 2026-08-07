3c9ab15ad2a5e6922e2954901f1207ff6636079c
Spherelim <mau.ortiz350@gmail.com>
2026-08-07 14:55:19 -0600

    UI: Actualizar Ventanas
    - Se realizo codigo para la navegación de paginas
    
    Se Agrego texto y cambio de estilo para verificaciónd e loscambios

---
+++ src/views/Principal.jsx
@@ -0,0 +1,12 @@
+import '../style/Principal.css'
+
+export default function Principal() {
+    return(
+        <div className="principal">
+            <div className="principal-container">
+                <h1 className="principal-title"></h1>
+                <p className="principal-description">Soy un desarrollador web apasionado por crear experiencias digitales únicas y funcionales. Explora mis proyectos y descubre cómo puedo ayudarte a llevar tus ideas al siguiente nivel.</p>
+            </div>
+        </div>
+    )
+}
