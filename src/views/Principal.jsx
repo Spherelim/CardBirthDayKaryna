76d9fee28efc464a4c3b10f4b2f016966beb11d2
Spherelim <mau.ortiz350@gmail.com>
2026-08-07 19:35:06 -0600

    Feat: Agregar Reloj y Imagenes Prueva
    - se integro un reloj para tenerlo como cronometro
    
    - se agregaron imagenes para tenerlos de referencia
    
    - al dar Click al personaje cambia de imagen

--- src/views/Principal.jsx
+++ src/views/Principal.jsx
@@ -1,11 +1,52 @@
+import { useEffect, useState } from 'react'
 import '../style/Principal.css'
 
+import Espera from '/public/Sonic.gif'
+import Baile from '/public/SonicBailando.gif'
+
 export default function Principal() {
+
+    const imagenes = [
+        Espera,
+        Baile
+    ]
+
+    const [imagenActual, setImagenActual] = useState(Espera)
+
+    const cambiarImagen = () => {
+        const indiceAleatorio = Math.floor(Math.random() * imagenes.length)
+
+        setImagenActual(imagenes[indiceAleatorio])
+    }
+
+    const [hora,setHora] = useState(new Date())
+
+    useEffect(()=>{
+
+        const intervalo = setInterval(() => {
+            setHora(new Date())
+        },1000)
+
+        return () => clearInterval(intervalo)
+
+    },[])
+
+    const horaFormateada = hora.toLocaleTimeString('es-MX',{
+        hour:'2-digit',
+        minute:'2-digit',
+        second:'2-digit',
+        hour12: true
+    })
+
     return(
         <div className="principal">
             <div className="principal-container">
-                <h1 className="principal-title"></h1>
-                <p className="principal-description">Soy un desarrollador web apasionado por crear experiencias digitales únicas y funcionales. Explora mis proyectos y descubre cómo puedo ayudarte a llevar tus ideas al siguiente nivel.</p>
+                <div id="reloj" class="reloj">{horaFormateada}</div>  
+                
+                <div className='Character'>
+                    <img src={imagenActual} alt="esperando..." onClick={cambiarImagen}/>
+                </div>
+
             </div>
         </div>
     )
