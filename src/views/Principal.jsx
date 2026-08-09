824fdfd135e5f974fa927dcf3b96f2e5624ff127
Spherelim <mau.ortiz350@gmail.com>
2026-08-08 19:25:16 -0600

    feat:Agregar Imagenes y Funciones
    - Se agrego una carpeta de Imagenes de acciones
    
    se implemento la Funcion de muerte y de Daño

--- src/views/Principal.jsx
+++ src/views/Principal.jsx
@@ -1,22 +1,58 @@
 import { useEffect, useState } from 'react'
 import '../style/Principal.css'
 
-import Espera from '/public/Sonic.gif'
-import Baile from '/public/SonicBailando.gif'
+import Espera from '/public/yo/Esperando.png'
+import Golpe_1 from '/public/yo/Golpe_1.png'
+import Golpe_2 from '/public/yo/Golpe_2.png'
+import Muelto from '/public/yo/Muelto.png'
 
 export default function Principal() {
 
     const imagenes = [
-        Espera,
-        Baile
+        Golpe_1,
+        Golpe_2
     ]
 
+    const Mensajes = [
+        "AUCH!!",
+        "AUU!!",
+        "OYE!!",
+        "¡¿POR QUE ME PEGAS?!",
+        "¡¿QUE TE HE HECHO?!"
+    ]
+
+    const [Golpes , setGolpes] = useState(0)
     const [imagenActual, setImagenActual] = useState(Espera)
+    const [muerto, setMuerto] = useState(false)
+    const [mensaje, setMensaje] = useState("Esperando tú Cumpleaños")
 
     const cambiarImagen = () => {
         const indiceAleatorio = Math.floor(Math.random() * imagenes.length)
 
+        const indiceMensaje = Math.floor(Math.random() * Mensajes.length)
+
         setImagenActual(imagenes[indiceAleatorio])
+        setMensaje(Mensajes[indiceMensaje])
+
+        if(!muerto)
+            setGolpes(Golpes + 1)
+                
+        if(Golpes + 1 >= 20){
+            setImagenActual(Muelto)
+            setMensaje("...")
+            setMuerto(true)
+        }
+        else{
+            // un pequeño tiempo
+            setTimeout(() => {
+                setImagenActual(Espera)
+            }, 300)
+
+            setTimeout(() => {
+                setMensaje("Esperando tú Cumpleaños")
+            }, 500)
+        }
+
     }
 
     const [hora,setHora] = useState(new Date())
@@ -39,14 +75,15 @@ export default function Principal() {
     })
 
     return(
-        <div className="principal">
+        <div className={`principal ${muerto ? 'muerto': ''}`}>
             <div className="principal-container">
-                <div id="reloj" class="reloj">{horaFormateada}</div>  
+                <div id="reloj" className="reloj">{horaFormateada}</div>  
                 
                 <div className='Character'>
-                    <img src={imagenActual} alt="esperando..." onClick={cambiarImagen}/>
+                    <img src={imagenActual} alt="esperando..." onClick={cambiarImagen} draggable="false"/>
                 </div>
 
+                <h2>"{mensaje}"</h2>
             </div>
         </div>
     )
